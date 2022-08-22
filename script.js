61fdd6f4a537a98b806ad3aac83d3f890f421231
rauserx <44473351+rauserx@users.noreply.github.com>
2022-08-22 19:41:17 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const imagen = document.getElementById("capa1");
+
+imagen.addEventListener("click",(e)=>{
+    let dieteticaurl = "https://wa.me/541136048804/?text=Hola todo piola"
+    window.open(dieteticaurl)
+
+
+
+});
+
+
